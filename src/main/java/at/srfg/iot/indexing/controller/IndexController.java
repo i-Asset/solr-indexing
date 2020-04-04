9a58cced026ab6afe51616feebfe1bff65c75579
Dietmar Glachs <dglachs@gmail.com>
2020-04-04 13:06:11 +0200

    @deprecated

--- src/main/java/at/srfg/iot/indexing/controller/IndexController.java
+++ src/main/java/at/srfg/iot/indexing/controller/IndexController.java
@@ -36,6 +36,7 @@ import io.swagger.annotations.ApiParam;
 
 //@CrossOrigin
 @RestController
+@Deprecated
 @Api(value = "Index Controller",
 		description = "Search API to perform Solr operations on indexed parties (organizations), items, item-properties, "
 				+ "property-codes and classes (item categories)")
