7c672df58710e24b2651c6d982b7203aef6c21fa
Dietmar Glachs <dglachs@gmail.com>
2020-04-04 13:05:03 +0200

    ensure credentials with solrj

--- src/main/java/at/srfg/iot/indexing/service/config/SolrContext.java
+++ src/main/java/at/srfg/iot/indexing/service/config/SolrContext.java
@@ -4,9 +4,10 @@ import java.util.Collections;
 
 import javax.annotation.Resource;
 
+import org.apache.http.auth.UsernamePasswordCredentials;
 import org.apache.solr.client.solrj.SolrClient;
 import org.apache.solr.client.solrj.impl.CloudSolrClient;
-import org.apache.solr.client.solrj.impl.HttpSolrClient;
+//import org.apache.solr.client.solrj.impl.HttpSolrClient;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,7 +15,9 @@ import org.springframework.core.env.Environment;
 import org.springframework.data.solr.repository.config.EnableSolrRepositories;
 import org.springframework.data.solr.server.SolrClientFactory;
 import org.springframework.data.solr.server.support.HttpSolrClientFactory;
+import org.springframework.util.StringUtils;
 
+import at.srfg.iot.indexing.solr.HttpSolrClient;
 import at.srfg.iot.indexing.solr.schema.SolrTemplate;
 
 
@@ -31,12 +34,17 @@ public class SolrContext {
 	@Bean
 	public SolrClientFactory solrClientFactory() {
 		String host = environment.getProperty("solr.host");
-		
+		String user = environment.getProperty("solr.user");
+		String pass = environment.getProperty("solr.password");
+		if (! (StringUtils.isEmpty(user) || StringUtils.isEmpty(pass))) {
+			return new HttpSolrClientFactory(solrClient(host), new UsernamePasswordCredentials(user, pass), "BASIC");
+		}
 		return new HttpSolrClientFactory(solrClient(host));
 	}
 	@Bean
 	public SolrClient solrClient(@Value("${solr.host}") String solrHost) {
 		if ( solrHost.startsWith("http")) {
+//			return new MySolrClient.Builder(solrHost).build();
 			return new HttpSolrClient.Builder(solrHost).build();
 		}
 		else {
@@ -48,5 +56,4 @@ public class SolrContext {
 	public SolrTemplate solrTemplate() {
 		return new SolrTemplate(solrClientFactory());
 	}
-
 }
